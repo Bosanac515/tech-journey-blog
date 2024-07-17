318fbd30cc799a12e1a829e2459d947e19ce73e9
halal mode <adenm0918@gmail.com>
2024-07-17 20:45:21 +0000

    changed alottttt

---
+++ posts/Best-poker-hands.md
@@ -0,0 +1,71 @@
+---
+title: 'Best Poker Hands'
+date: '2024-07-17'
+---
+
+![Best Hands](/workspace/tech-journey-blog/public/images/Poker-Hand-Rankings.jpg)
+
+Understanding Poker Hand Rankings: Your Guide to Mastering the Game
+Poker is a game of skill, strategy, and a bit of luck. One of the fundamental aspects of becoming a proficient poker player is understanding hand rankings. Knowing which hands outrank others can be the difference between a big win and a significant loss. In this blog post, we'll break down the best poker hand rankings to help you master the game and increase your chances of winning.
+
+The Basics of Poker Hand Rankings
+In poker, hands are ranked based on the probability of being dealt those hands in a standard 52-card deck. The rarer the hand, the higher its ranking. Here are the standard poker hand rankings from highest to lowest:
+
+1. Royal Flush
+Description: The highest possible hand in poker, the Royal Flush consists of the Ace, King, Queen, Jack, and Ten, all of the same suit.
+
+Example: A♠ K♠ Q♠ J♠ 10♠
+
+2. Straight Flush
+Description: Five consecutive cards of the same suit. If two players have a Straight Flush, the hand with the highest top card wins.
+
+Example: 9♣ 8♣ 7♣ 6♣ 5♣
+
+3. Four of a Kind
+Description: Four cards of the same rank and one unrelated card. Also known as "quads."
+
+Example: Q♠ Q♦ Q♣ Q♥ 3♠
+
+4. Full House
+Description: Three cards of one rank and two cards of another rank. In the event of a tie, the player with the higher three of a kind wins.
+
+Example: K♣ K♦ K♠ 4♠ 4♣
+
+5. Flush
+Description: Five cards of the same suit, not in consecutive order. The highest card in the flush determines the winner in case of a tie.
+
+Example: J♠ 8♠ 5♠ 3♠ 2♠
+
+6. Straight
+Description: Five consecutive cards of different suits. The highest card in the straight determines the winner in case of a tie.
+
+Example: 10♠ 9♣ 8♦ 7♠ 6♥
+
+7. Three of a Kind
+Description: Three cards of the same rank and two unrelated side cards. Also known as "trips" or "set."
+
+Example: 8♣ 8♦ 8♠ K♠ 3♥
+
+8. Two Pair
+Description: Two cards of one rank, two cards of another rank, and one unrelated card. The highest pair wins in case of a tie.
+
+Example: 7♠ 7♦ 5♣ 5♠ 2♦
+
+9. One Pair
+Description: Two cards of the same rank and three unrelated side cards. The highest pair wins in case of a tie.
+
+Example: 9♠ 9♥ A♣ J♠ 4♦
+
+10. High Card
+Description: If no player has any of the above hands, the hand with the highest card wins. If necessary, the second highest, third highest, and so on, can be used to break ties.
+
+Example: A♠ 10♦ 7♣ 6♠ 2♥
+
+Tips for Remembering Hand Rankings
+Practice Regularly: The more you play poker, the more familiar you will become with hand rankings.
+Use a Cheat Sheet: Keep a hand ranking cheat sheet nearby when you are learning or playing online poker.
+Group Similar Hands: Recognize patterns in the rankings. For instance, remember that a Full House beats a Flush, which in turn beats a Straight.
+Conclusion
+Understanding poker hand rankings is essential for anyone serious about improving their game. Whether you're playing casually with friends or aiming to compete in tournaments, knowing the hierarchy of hands will help you make better decisions and increase your chances of winning.
+
+Stay tuned to Luke's Blog for more tips, strategies, and insights into the world of poker. Happy playing!
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,72 @@
-import Link from 'next/link'
-import Date from '@/components/Date'
-
-import {getSortedPostsData} from '@/lib/posts'
+import Link from 'next/link';
+import Date from '@/components/Date';
+import { getSortedPostsData } from '@/lib/posts';
 import Image from "next/image";
 
 type AllPostsData = {
-  date: string
-  title: string
-  id: string
-}[]
+  date: string;
+  title: string;
+  id: string;
+}[];
 
 // Change this url to change the image!
 const imageUrl = '/images/seth-snorlax.webp';
+const leftBannerUrl = '/images/minecraft-left-banner.webp';
+const rightBannerUrl = '/images/minecraft-right-banner.webp';
 
 export default function Home() {
-  const allPostsData: AllPostsData = getSortedPostsData()
+  const allPostsData: AllPostsData = getSortedPostsData();
 
   return (
-      <div>
-        <section>
-          <p className={'prose'}>
-            Hey I&apos;m Aden. This is my site.
-          </p>
-          <div className={'my-4'}>
-            <i>
-              Check out the repo{''}
-                <Link href={'https://lukes-secrets-okvyv49m5-bosanac515s-projects.vercel.app/'}>
-                  <button className={'btn btn-primary'}>
-                      <span className={'text-primary-content'}>
-                        here
-                      </span>
-                  </button>
-                </Link>
-            </i>
-          </div>
-        </section>
-
-        <div className={'my-4'}>
-          <Image className={'rounded'} src={imageUrl} alt={'My Image'} width={500}
-                 height={500}/>
+    <div className="relative">
+      <section className="prose">
+        <p>Hey I&apos;m Aden. This is my site.</p>
+        <div className="my-4">
+          <i>
+            Check out the repo{' '}
+            <Link href={'https://lukes-secrets-okvyv49m5-bosanac515s-projects.vercel.app/'}>
+              <button className="btn btn-primary">
+                <span className="text-primary-content">
+                  here
+                </span>
+              </button>
+            </Link>
+          </i>
         </div>
+      </section>
 
-        <section className={'prose'}>
-          <h2>Blog</h2>
-          <ul>
-            {allPostsData.map(({id, date, title}) => (
-                <li key={id}>
-                  <div>
-                    <Link href={`/posts/${id}`}>{title}</Link>
-                    <br/>
-                    <small>
-                      <Date dateString={date}/>
-                    </small>
-                  </div>
-                </li>
-            ))}
-          </ul>
-        </section>
+      <div className="my-4">
+        <Image className="rounded" src={imageUrl} alt="My Image" width={500} height={500} />
       </div>
-  )
+
+      <section className="prose">
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              <div>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small>
+                  <Date dateString={date} />
+                </small>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className="prose my-8">
+        <h2>Bounty</h2>
+        <p>🔥💀 Bounty for the following cards: 9 of ♠️, King of ♥️, Ace of ♥️ 💀🔥</p>
+      </section>
+
+      <div className="absolute top-0 left-0 w-1/4 h-full">
+        <Image src={leftBannerUrl} alt="Left Banner" layout="fill" objectFit="cover" />
+      </div>
+      <div className="absolute top-0 right-0 w-1/4 h-full">
+        <Image src={rightBannerUrl} alt="Right Banner" layout="fill" objectFit="cover" />
+      </div>
+    </div>
+  );
 }
